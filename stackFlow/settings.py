984b5eac83e48a22df77c7a2d1cd82f99337876d
Pierre <pierre.nogaro@gmail.com>
2025-04-07 19:06:15 +0200

    cors

--- stackFlow/settings.py
+++ stackFlow/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7jp0*!o6=-2a(q#(r-9v2y^z*y@$^po00rkl%7lr2r)tu-21oa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'website.apps.WebsiteConfig',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
 ]
 
 ## rest framework
@@ -66,8 +67,7 @@ ROOT_URLCONF = 'stackFlow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -138,10 +138,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ## simple jwt configurations
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 ## ZOHO MAIL :
@@ -153,3 +154,31 @@ DEFAULT_FROM_EMAIL = 'pierrenogaro@zohomail.eu'
 EMAIL_HOST_PASSWORD = '7D9mreCh6sfb'
 EMAIL_USE_TLS = True
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+     "http://localhost:8080",
+     "http://127.0.0.1:8080",
+     "http://localhost:5173",
+     "http://127.0.0.1:5173",
+ ]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
